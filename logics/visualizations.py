670bd29935e0536312ae39ef713f20ac2c54d81c
Noa <noamishraki2@gmail.com>
2023-07-02 09:26:01 -0700

    hierarchy

--- logics/visualizations.py
+++ logics/visualizations.py
@@ -1,34 +1,26 @@
+from typing import List
+
 import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
-import seaborn as sns
-import numpy as np
 from sklearn.linear_model import LinearRegression
+import numpy as np
 
-#C:\Users\user\Documents\GitHub\MemoryDiary\Intrusions+Diary+-+Daily+summery_26+June+2023_15.17.xlsx
 
-file_name =r'139_Daily_Diary_Summary.xlsx'
-treatment_dates = r'dates139.xlsx'
-
-def plot_graph(file_name: Path, treatment_dates: Path, count_of_interest: str):
-    print(treatment_dates)
-    df = pd.read_excel(file_name)
-    #df = df.drop(df.index[0])
-    treatment_dates = pd.read_excel(treatment_dates)
-
+def plot_graph(df: pd.DataFrame, treatment_dates: List, count_of_interest: str):
+    df = df.drop(df.index[0])
     # Assuming you have imported necessary libraries and loaded the data frame (df)
-    colors = ['green' if date in treatment_dates.values else 'blue' for date in df['Date']]
+    colors = ['green' if date in treatment_dates else 'blue' for date in df['Date']]
 
-    plt.bar(df['Date'].unique(), df[count_of_interest], color=colors)
-    
-    i=1
+    plt.bar(df['Date'], df[count_of_interest], color=colors)
+    i = 1
     for date, treatment in zip(df['Date'], df[count_of_interest]):
-        if date in treatment_dates.values:
+        if date in treatment_dates:
             plt.text(date, treatment, i, ha='center', va='bottom')
-            i+=1
-        
+            i += 1
+
     # Convert the date column to numeric values for regression
-    date_values = np.arange(len(df['Date'].unique()))
+    date_values = np.arange(len(df['Date']))
 
     # Reshape the date values to fit the input requirements of LinearRegression
     X1 = date_values.reshape(-1, 1)
@@ -44,21 +36,20 @@ def plot_graph(file_name: Path, treatment_dates: Path, count_of_interest: str):
     # Plot the trend line
     plt.plot(df['Date'], trend_y1, color='red', label='Trend Line')
 
-    plt.ylim(0,None)
+    plt.ylim(0, None)
     plt.xticks(rotation=90)
     # Add labels and legends
     plt.xlabel('Date')
     plt.ylabel(count_of_interest)
+    plt.title(count_of_interest)
     plt.legend()
+
     # Display the plot
     plt.show()
-    
-def average_per_week(file_name: Path, treatment_dates: Path, count_of_interest: str):
 
-    df = pd.read_excel(file_name)
+
+def average_per_week(df: pd.DataFrame, count_of_interest: str):
     df = df.drop(df.index[0])
-    print(df['Date'])
-    treatment_dates = pd.read_excel(treatment_dates)
     df['Date'] = pd.to_datetime(df['Date'])
     df.set_index(df['Date'], inplace=True)
 
@@ -67,20 +58,14 @@ def average_per_week(file_name: Path, treatment_dates: Path, count_of_interest: 
 
     # Reset the index to make 'Date' a column again
     weekly_average = weekly_average.reset_index()
-    #dates = weekly_average['Date'].tolist()
-    weeks= weekly_average['Date']
+    # dates = weekly_average['Date'].tolist()
+    weeks = weekly_average['Date']
     averages = weekly_average[count_of_interest].tolist()
-    
+
     # Create the bar plot
     plt.bar(weekly_average['Date'], weekly_average[count_of_interest], width=6.5)
     plt.xticks(weeks, rotation=90)
     plt.xlabel('Week')
-    plt.ylabel('Average Count_Target')
-    plt.title('Weekly Average Count_Target')
+    plt.ylabel(f'Average {count_of_interest}')
+    plt.title(f'Weekly Average {count_of_interest}')
     plt.show()
-
-
-
-average_per_week(file_name, treatment_dates, 'Count_Target')
-average_per_week(file_name, treatment_dates, 'Count_Nontarget')
-average_per_week(file_name, treatment_dates, 'Count_Total')
