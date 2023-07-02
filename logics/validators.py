c15e52576da4616edb24bdc5eff7229035bf72a8
Noa <noamishraki2@gmail.com>
2023-07-02 10:47:26 -0700

    getting started with unittests

--- logics/validators.py
+++ logics/validators.py
@@ -1,11 +1,9 @@
 from typing import List
-import pandas as pd
 from datetime import datetime
 
 from errors import DateBeforeDataDates
 
 
-def validate_range(dates: List, df: pd.DataFrame):
-    smallest_date = df['Date'].min()
+def validate_range(dates: List, smallest_date: datetime.date):
     if not all(date > smallest_date for date in dates):
         raise DateBeforeDataDates
