bc38e91763b27e5637a306f6c011bb703a30be3c
Noa <noamishraki2@gmail.com>
2023-07-02 08:41:55 -0700

    date validation

--- validators.py
+++ validators.py
@@ -1,11 +1,10 @@
 from typing import List
 import pandas as pd
-
+from datetime import datetime
 from errors import DateBeforeDataDates
 
 
 def validate_range(dates:List, df: pd.DataFrame):
-    # smallest_date = df['Date'].min()
-    # if not all(date_str > str(smallest_date) for date_str in dates):
-    #     raise DateBeforeDataDates
-    pass
+    smallest_date = df['Date'].min()
+    if not all(datetime.strptime(date, '%Y/%m/%d').date() > smallest_date for date in dates):
+        raise DateBeforeDataDates
